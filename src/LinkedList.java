5545513af063b66e17e9ce83870aeb883108ed03
punpat2000 <saint12302@gmail.com>
2020-01-30 13:56:19 +0700

    first commit

---
+++ src/LinkedList.java
@@ -0,0 +1,194 @@
+class ListNode {
+	// Constructors
+	ListNode(Object theElement) {
+		this(theElement, null);
+	}
+
+	ListNode(Object theElement, ListNode n) {
+		data = theElement;
+		nextNode = n;
+	}
+
+	// Friendly data; accessible by other package routines
+	Object data;
+	ListNode nextNode;
+}
+
+public class LinkedList {
+	ListNode header;
+
+	public LinkedList() {
+		header = new ListNode(null);
+	}
+
+	public boolean isEmpty() {
+		return header.nextNode == null;
+	}
+
+	/** make the list empty. */
+	public void makeEmpty() {
+		header.nextNode = null;
+	}
+
+	public void insert(Object value, Iterator p) throws Exception {
+		if (p == null || !(p instanceof MyListIterator))
+			throw new Exception();
+		MyListIterator p2 = (MyListIterator) p;
+		if (p2.currentNode == null)
+			throw new Exception();
+		ListNode n = new ListNode(value, p2.currentNode.nextNode);
+		p2.currentNode.nextNode = n;
+	}
+
+	public int find(Object value) throws Exception {
+		Iterator itr = new MyListIterator(header);
+		int index = -1;
+		while (itr.hasNext()) {
+			Object v = itr.next();
+			index++;
+			if (v.equals(value))
+				return index; // return the position of value.
+		}
+		return -1;
+	}
+
+	public Iterator findPrevious(Object value) throws Exception {
+		Iterator itr1 = new MyListIterator(header);
+		Iterator itr2 = new MyListIterator(header);
+		if (!itr2.hasNext())
+			return null;
+		Object currentData = itr2.next();
+		while (!currentData.equals(value) && itr2.hasNext()) {
+			currentData = itr2.next();
+			itr1.next();
+		}
+		if (currentData.equals(value))
+			return itr1;
+		return null;
+
+	}
+
+	public void remove(Iterator p) {
+		if (p == null || !(p instanceof MyListIterator))
+			return;
+		MyListIterator p2 = (MyListIterator) p;
+		if (p2.currentNode == null || p2.currentNode.nextNode == null)
+			return;
+		p2.currentNode.nextNode = p2.currentNode.nextNode.nextNode;
+	}
+
+	public void remove(Object value) throws Exception {
+		Iterator p = findPrevious(value);
+		if (p == null)
+			return;
+		remove(p);
+	}
+
+	// Returns the number of data stored in this list.
+	// To be completed by students.
+	public int size() throws Exception {
+		Iterator itr = new MyListIterator(header);
+		if (!itr.hasNext())
+			return 0;
+		int count = 0;
+		while (itr.hasNext()) {
+			itr.next();
+			count++;
+		}
+		return count;
+	}
+
+	// Print each contact out, one by one.
+	// To be completed by students.
+	public void printList() throws Exception {
+		Iterator itr = new MyListIterator(header);
+		if (!itr.hasNext()) {
+			System.out.println("The list has no data.");
+			return;
+		}
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+
+	}
+
+	// Return iterator pointing to value that stores a given name, or null
+	// otherwise.
+	// To be completed by students.
+	public Iterator findPosition(String name) throws Exception {
+		Iterator itr = new MyListIterator(header);
+		if (!itr.hasNext())
+			return null;
+
+		while (itr.hasNext()) {
+			String temp = ((Contact) itr.next()).name;
+			if (temp.equals(name))
+				return itr;
+		}
+		return null;
+	}
+
+	// add a new contact to the list the contact must be added in such a way that
+	// each contact is sorted by name, in alphabetical order.
+
+	public void add(Object c) throws Exception {
+		if (c == null || !(c instanceof Contact))
+			return;
+		Contact c1 = (Contact) c;
+		Iterator itr = new MyListIterator(this.header);
+		if (!itr.hasNext()) {
+			this.insert(c1, (MyListIterator) itr);
+			return;
+		}
+
+		while (itr.hasNext()) {
+			Contact temp1 = (Contact) itr.next();
+			String temp2 = temp1.name;
+			if (c1.name.compareTo(temp2) <= 0) {
+				this.insert(c1, this.findPrevious(temp1));
+				return;
+			}
+		}
+		this.insert(c1, (MyListIterator) itr);
+	}
+
+	// Remove the contact pointed by the iterator, i, and then returns an iterator
+	// pointing to the next contact.
+	// If the removed contact is the last one, return the iterator pointing to the
+	// first contact
+	// (if there is no first contact, make the iterator point to header).
+	// If i is marking an illegal position that cannot be removed, just return null.
+	// To be completed by students.
+	public Iterator removeAt(Iterator i) throws Exception {
+		if (i == null)
+			return null;
+		MyListIterator itr = new MyListIterator(this.header);
+		if (!itr.hasNext())
+			return null;
+		while(itr.hasNext()) {
+			System.out.println(((MyListIterator) i).currentNode.data+"before");
+			
+			if (itr.currentNode.nextNode == ((MyListIterator) i).currentNode) {
+				System.out.println(itr.currentNode.data+"before");
+				//this.printList();
+				this.remove(itr);
+				System.out.println(itr.currentNode.data+"removedd");
+				if(!itr.hasNext()) {
+					Iterator itr2 = new MyListIterator(this.header);
+					if(!itr2.hasNext()) return itr2;
+					itr2.next();
+					return itr2;
+				}
+				//itr.next();
+				System.out.println();
+				//this.printList();
+				System.out.println("here");
+				return itr;
+			}
+			itr.next();
+		}
+
+		return null;
+	}
+
+}
